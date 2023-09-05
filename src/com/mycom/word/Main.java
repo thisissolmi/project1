f1dbd306bb282de31acc4bb9abe227cc8f43696f
oksk6685@handong.ac.kr <oksk6685@handong.ac.kr>
2023-09-06 02:21:08 +0900

    "commit test"

--- src/com/mycom/word/Main.java
+++ src/com/mycom/word/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         new WordManager().start(); //객체생성
-        //System.out.println("HELLO");
+        //System.out.println("HELLO"); 와하우
     }
 
 }
